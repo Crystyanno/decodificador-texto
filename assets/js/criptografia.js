cd09db964b7d819c7bee7fcf9a66e986c168a09e
Crystyanno Souza <crystyanno.alves@gmail.com>
2024-07-05 20:48:54 -0300

    up criptografia

--- assets/js/criptografia.js
+++ assets/js/criptografia.js
@@ -1,50 +1,31 @@
 function codifica(texto) {
-    var caracteres = texto.value.split("");
-
-    caracteres.forEach(function(item, i) {
-        if(item == "a") {
-            caracteres[i] = "ai";
-
-        } else if(item == "e") {
-            caracteres[i] = "enter";
-
-        } else if(item == "i") {
-            caracteres[i] = "imes";
-
-        } else if(item == "o") {
-            caracteres[i] = "ober";
-
-        } else if(item == "u") {
-            caracteres[i] = "ufat";
-        }
-    })
-    return caracteres.join("");
+    let encryptedText = '';
+    for (let i = 0; i < texto.length; i++) {
+        encryptedText += String.fromCharCode(texto.charCodeAt(i) + 3);
+    }
+    return encryptedText;
 }
 
 function decodifica(texto) {
-    var codigo = [["a", "ai"], ["e", "enter"], ["i", "imes"], ["o", "ober"], ["u", "ufat"]];
-    texto = texto.value;
-
-    for(var i = 0; i < codigo.length; i++) {
-        if(texto.includes(codigo[i][1])) {
-            texto = texto.replaceAll(codigo[i][1], codigo[i][0]);
-        }
+    let decryptedText = '';
+    for (let i = 0; i < texto.length; i++) {
+        decryptedText += String.fromCharCode(texto.charCodeAt(i) - 3);
     }
-    return texto;
+    return decryptedText;
 }
 
 function escreveCodificado() {
     var btnCopiar = document.querySelector(".btn-copiar");
     btnCopiar.classList.remove("invisivel");
     
-    resultado.textContent = codifica(inputTexto);
+    resultado.textContent = codifica(inputTexto.value);
 }
 
 function escreveDecodificado() {
     var btnCopiar = document.querySelector(".btn-copiar");
     btnCopiar.classList.remove("invisivel");
 
-    resultado.textContent = decodifica(inputTexto);
+    resultado.textContent = decodifica(inputTexto.value);
 }
 
 var inputTexto = document.querySelector(".input-texto");
@@ -78,5 +59,5 @@ btnDecodificar.onclick = function() {
     }
     
     escreveDecodificado();
-    mensagensErro.innerHTML = "";
+    mensagensErro.innerHTML = "";
 }
