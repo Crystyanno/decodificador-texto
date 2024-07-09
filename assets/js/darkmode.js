4413c7f697a4d9589b5b26a94555ad8879078c86
Crystyanno Souza <crystyanno.alves@gmail.com>
2024-07-08 21:02:19 -0300

    insert dark-mode

---
+++ assets/js/darkmode.js
@@ -0,0 +1,8 @@
+const dark = document.querySelector('.dark');
+const darkmode = document.querySelector('.dark-mode');
+
+btn.Codificar.onclick = function()
+{
+    this.classList.toggle('active')
+    CSSContainerRule.classList.toggle('active')
+}
